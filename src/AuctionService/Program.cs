0e81497f897db27655e7d219e7dcf2e04118d747
Andrew Lobban <andrewglobban@gmail.com>
2024-06-04 17:12:48 -0500

    Add Service Bus Message Outbox

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -11,9 +11,17 @@ builder.Services.AddDbContext<AuctionDbContext>(opt => {
 });
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-builder.Services.AddMassTransit(XmlConfigurationExtensions =>
+builder.Services.AddMassTransit(x =>
 {
-  XmlConfigurationExtensions.UsingRabbitMq((context, cfg) =>
+  x.AddEntityFrameworkOutbox<AuctionDbContext>(o => 
+  {
+    o.QueryDelay = TimeSpan.FromSeconds(10);
+
+    o.UsePostgres();
+    o.UseBusOutbox();
+  });
+
+  x.UsingRabbitMq((context, cfg) =>
   {
     cfg.ConfigureEndpoints(context);
   });
