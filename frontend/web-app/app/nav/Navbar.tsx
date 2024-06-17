6bae5f0acf2ae42806b1730c60d9995187e8766b
Andrew Lobban <andrewglobban@gmail.com>
2024-06-17 13:34:17 -0500

    Add Search functionality

--- frontend/web-app/app/nav/Navbar.tsx
+++ frontend/web-app/app/nav/Navbar.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { AiOutlineCar } from 'react-icons/ai';
 
+import Search from './Search';
+
 export default function Navbar() {
   return (
     <header className="sticky top-0 z-50 flex justify-between bg-white p-5 items-center text-gray-800 shadow-md">
@@ -8,7 +10,7 @@ export default function Navbar() {
         <AiOutlineCar size={34} />
         <div>Auto Auction</div>
       </div>
-      <div>Search</div>
+      <Search />
       <div>Login</div>
     </header>
   );
