867a03466c318ab758b0b161bbe05b9ee1d085c8
Andrew Lobban <andrewglobban@gmail.com>
2024-06-18 11:54:12 -0500

    Add Filter functionality

--- frontend/web-app/app/auctions/Filters.tsx
+++ frontend/web-app/app/auctions/Filters.tsx
@@ -1,8 +1,10 @@
-import { useParamsStore } from '@/hooks/useParamsStore';
 import { Button } from 'flowbite-react';
 import React from 'react';
 import { AiOutlineClockCircle, AiOutlineSortAscending } from 'react-icons/ai';
-import { BsFillStopCircleFill } from 'react-icons/bs';
+import { BsFillStopCircleFill, BsStopwatchFill } from 'react-icons/bs';
+import { GiFinishLine, GiFlame } from 'react-icons/gi';
+
+import { useParamsStore } from '@/hooks/useParamsStore';
 
 const pageSizeButtons = [4, 8, 12];
 
@@ -24,15 +26,49 @@ const orderButtons = [
   },
 ];
 
+const filterButtons = [
+  {
+    label: 'Live Auctions',
+    icon: GiFlame,
+    value: 'live',
+  },
+  {
+    label: 'End < 6 hours',
+    icon: GiFinishLine,
+    value: 'endingSoon',
+  },
+  {
+    label: 'Complete',
+    icon: BsStopwatchFill,
+    value: 'finished',
+  },
+];
+
 export default function Filters() {
   const pageSize = useParamsStore((state) => state.pageSize);
   const setParams = useParamsStore((state) => state.setParams);
   const orderBy = useParamsStore((state) => state.orderBy);
+  const filterBy = useParamsStore((state) => state.filterBy);
 
   return (
     <div className="flex justify-between items-center mb-4">
       <div>
-        <span className="uppercase text-sm text-gray-500 mr-2">Page size</span>
+        <span className="uppercase text-sm text-gray-500 mr-2">Filter by</span>
+        <Button.Group>
+          {filterButtons.map(({ label, icon: Icon, value }) => (
+            <Button
+              key={value}
+              onClick={() => setParams({ filterBy: value })}
+              color={`${filterBy === value ? 'red' : 'gray'}`}
+            >
+              <Icon className="mr-3 h-4 w-4" />
+              {label}
+            </Button>
+          ))}
+        </Button.Group>
+      </div>
+      <div>
+        <span className="uppercase text-sm text-gray-500 mr-2">Sort by</span>
         <Button.Group>
           {orderButtons.map(({ label, icon: Icon, value }) => (
             <Button
