852c44daee1c4826bd2a402f802c67b605deac31
Andrew Lobban <andrewglobban@gmail.com>
2024-06-18 12:14:11 -0500

    Handle zero results

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -10,6 +10,7 @@ import AppPagination from '@/app/components/AppPagination';
 import { getData } from '../actions/auctionActions';
 import Filters from './Filters';
 import { useParamsStore } from '@/hooks/useParamsStore';
+import EmptyFilter from '../components/EmptyFilter';
 
 export default function Listings() {
   const [data, setData] = useState<PagedResult<Auction>>();
@@ -41,18 +42,24 @@ export default function Listings() {
   return (
     <>
       <Filters />
-      <div className="grid grid-cols-4 gap-6">
-        {data.results.map((auction) => (
-          <AuctionCard auction={auction} key={auction.id} />
-        ))}
-      </div>
-      <div className="flex justify-center mt-4">
-        <AppPagination
-          currentPage={params.pageNumber}
-          pageCount={data.pageCount}
-          pageChanged={setPageNumber}
-        />
-      </div>
+      {data.totalCount === 0 ? (
+        <EmptyFilter showReset />
+      ) : (
+        <>
+          <div className="grid grid-cols-4 gap-6">
+            {data.results.map((auction) => (
+              <AuctionCard auction={auction} key={auction.id} />
+            ))}
+          </div>
+          <div className="flex justify-center mt-4">
+            <AppPagination
+              currentPage={params.pageNumber}
+              pageCount={data.pageCount}
+              pageChanged={setPageNumber}
+            />
+          </div>
+        </>
+      )}
     </>
   );
 }
