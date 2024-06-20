e576c52afad7a66ca6a77d2cceeaa24979629a48
Andrew Lobban <andrewglobban@gmail.com>
2024-06-20 13:48:36 -0500

    Protect route

---
+++ frontend/web-app/middleware.ts
@@ -0,0 +1,10 @@
+import { pages } from 'next/dist/build/templates/app-page';
+
+export { default } from 'next-auth/middleware';
+
+export const config = {
+  matcher: ['/session'],
+  pages: {
+    signIn: '/api/auth/signin',
+  },
+};
