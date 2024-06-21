2251e0ea05edb729525aea88ef94f4288ebd6d37
Andrew Lobban <andrewglobban@gmail.com>
2024-06-21 15:42:59 -0500

    Create Auction Forms component

--- frontend/web-app/app/auctions/create/page.tsx
+++ frontend/web-app/app/auctions/create/page.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import Heading from '@/app/components/Heading';
+import AuctionForm from '../AuctionForm';
+
 export default function Create() {
   return (
-    <div>
-      <div>Create auction</div>
+    <div className="mx-auto max-w-[75%] shadow-lg p-10 bg-white rounded-lg">
+      <Heading
+        title="Sell your car!"
+        subtitle="Please enter the details of your car"
+      />
+      <AuctionForm />
     </div>
   );
 }
