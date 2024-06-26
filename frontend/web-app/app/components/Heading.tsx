5437e5bbe5edaa9cc7b7a46560b33acb8af35b62
Andrew Lobban <andrewglobban@gmail.com>
2024-06-26 11:37:04 -0500

    Fix class name typo

--- frontend/web-app/app/components/Heading.tsx
+++ frontend/web-app/app/components/Heading.tsx
@@ -9,7 +9,7 @@ type Props = {
 export default function Heading({ title, subtitle, center }: Props) {
   return (
     <div className={center ? 'text-center' : 'text-start'}>
-      <div className="text-sxl font-bold">{title}</div>
+      <div className="text-xl font-bold">{title}</div>
       <div className="font-light text-neutral-500 mt-2">{subtitle}</div>
     </div>
   );
